e1fa239e59149b8b652aa2fbe1eb9b8c787bf716
wuweijieqity <31516968+wuweijieqity@users.noreply.github.com>
2017-11-20 16:03:40 -0500

    Add files via upload

---
+++ src/main/java/com/nowcoder/service/ToutiaoService.java
@@ -0,0 +1,13 @@
+package com.nowcoder.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by weijie on 2017/6/26.
+ */
+@Service
+public class ToutiaoService {
+    public String say() {
+        return "This is from ToutiaoService";
+    }
+}
